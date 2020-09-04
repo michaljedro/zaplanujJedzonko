ccd4abf86bd9d9bfe26bfd2a123d605311a74152
Michał <michal.jedro@wp.pl>
2020-09-04 14:06:11 +0200

    changes from comments

--- app/js/script.js
+++ app/js/script.js
@@ -1,38 +1,38 @@
 
 var app = {};
 
-var btnPrev = document.querySelector('#prevPicture');
+var btnPrev = document.querySelector('#prev-picture');
 
-var btnNext = document.querySelector('#nextPicture');
+var btnNext = document.querySelector('#next-picture');
 
 var sliderDivs = document.querySelectorAll('.slider div');
 
-var item = Array.from(sliderDivs);
+var items = Array.from(sliderDivs);
 
 var currentImageIndex = 0;
 
-item[0].classList.add('visible');
+items[0].classList.add('visible');
 
 function nextSlide() {
-    item[currentImageIndex].classList.remove('visible');
-    if (currentImageIndex === item.length - 1) {
+    items[currentImageIndex].classList.remove('visible');
+    if (currentImageIndex === items.length - 1) {
         currentImageIndex = 0;
     } else {
         currentImageIndex++;
     }
-    item[currentImageIndex].classList.add('visible');
+    items[currentImageIndex].classList.add('visible');
 }
 
 btnNext.addEventListener('click', nextSlide);
 
 function prevSlide() {
-    item[currentImageIndex].classList.remove('visible');
+    items[currentImageIndex].classList.remove('visible');
     if (currentImageIndex === 0) {
-        currentImageIndex = item.length - 1;
+        currentImageIndex = items.length - 1;
     } else {
         currentImageIndex--;
     }
-    item[currentImageIndex].classList.add('visible');
+    items[currentImageIndex].classList.add('visible');
 }
 
 btnPrev.addEventListener('click', prevSlide);
