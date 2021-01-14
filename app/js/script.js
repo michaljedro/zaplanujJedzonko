edaa5bba72a851333f8660199dc76965b6f07e32
michaljedro <michal.jedro@wp.pl>
2021-01-14 16:47:56 +0100

    add js work

--- app/js/script.js
+++ app/js/script.js
@@ -1,47 +1,67 @@
+
 var app = {};
 
-var btnPrev = document.querySelector("#prev-picture");
+var btnPrev = document.querySelector('#prev-picture');
 
-var btnNext = document.querySelector("#next-picture");
+var btnNext = document.querySelector('#next-picture');
 
-var sliderDivs = document.querySelectorAll(".slider div");
+var sliderDivs = document.querySelectorAll('.slider div');
 
 var items = Array.from(sliderDivs);
 
 var currentImageIndex = 0;
 
-items[0].classList.add("visible");
+items[0].classList.add('visible');
 
 function nextSlide() {
-  items[currentImageIndex].classList.remove("visible");
-  if (currentImageIndex === items.length - 1) {
-    currentImageIndex = 0;
-  } else {
-    currentImageIndex++;
-  }
-  items[currentImageIndex].classList.add("visible");
+    items[currentImageIndex].classList.remove('visible');
+    if (currentImageIndex === items.length - 1) {
+        currentImageIndex = 0;
+    } else {
+        currentImageIndex++;
+    }
+    items[currentImageIndex].classList.add('visible');
 }
 
-btnNext.addEventListener("click", nextSlide);
+btnNext.addEventListener('click', nextSlide);
 
 function prevSlide() {
-  items[currentImageIndex].classList.remove("visible");
-  if (currentImageIndex === 0) {
-    currentImageIndex = items.length - 1;
-  } else {
-    currentImageIndex--;
-  }
-  items[currentImageIndex].classList.add("visible");
+    items[currentImageIndex].classList.remove('visible');
+    if (currentImageIndex === 0) {
+        currentImageIndex = items.length - 1;
+    } else {
+        currentImageIndex--;
+    }
+    items[currentImageIndex].classList.add('visible');
 }
 
-btnPrev.addEventListener("click", prevSlide);
+btnPrev.addEventListener('click', prevSlide);
 
 var btnVisible = document.querySelector('.list__items--modify');
 
 console.log(btnVisible);
 
-function show() {
-  var dashView = document.querySelector('.dashbord_view');
-  dashView.style.display = 'block';
-  // var indexView = document.
-}
+btnVisible.addEventListener('click', function(e) {
+e.preventDefault();
+var dashView = document.querySelector('.dashbord_view');
+var show = document.querySelector('.show');
+dashView.style.display = 'block';
+show.style.display = 'none';
+});
+
+var noneVisible = document.querySelector('.dashbordview__header--head');
+
+noneVisible.addEventListener('click', function(e) {
+var dashView = document.querySelector('.dashbord_view');
+var show = document.querySelector('.show');
+dashView.style.display = 'none';
+show.style.display = 'block';
+})
+// btnVisible.addEventListener('click', function(e) {
+//     e.preventDefault();
+//     var dashView = document.querySelector('.dashbord_view');
+//     var show = document.querySelector('.show');
+//     dashView.style.display = 'none';
+//     show.style.display = 'block';
+//     });
+
